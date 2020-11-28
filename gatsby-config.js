e1b2dcbdf89f21810739ae63d35674e9c867c386
ghemsley <ghemsley@protonmail.ch>
2020-11-28 11:04:32 -0600

    Update CSP settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -399,7 +399,7 @@ module.exports = {
         disableOnDev: true,
         reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
         mergeScriptHashes: true, // you can disable scripts sha256 hashes
-        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeStyleHashes: false, // you can disable styles sha256 hashes
         mergeDefaultDirectives: true,
         directives: {
           "script-src":
