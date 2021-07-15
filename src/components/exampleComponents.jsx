792ce326cd73493ee9deda146d956b36f35b517d
ghemsley <ghemsley@protonmail.ch>
2021-07-15 17:02:54 -0500

    Try loadable components

--- src/components/exampleComponents.jsx
+++ src/components/exampleComponents.jsx
@@ -1,4 +1,5 @@
 import React, { Component, useState, useEffect } from "react"
+import loadable from "@loadable/component"
 
 export class Dog extends Component {
   constructor(props) {
@@ -60,47 +61,6 @@ export const FunctionalDog = props => {
   )
 }
 
-export const TitleChanger = () => {
-  const [title, setTitle] = useState(document.title)
-  const [value, setValue] = useState("")
-
-  useEffect(() => {
-    document.title = title
-  }, [title])
-
-  const handleSubmit = event => {
-    event.preventDefault()
-    setTitle(value)
-  }
-
-  return (
-    <div
-      style={{
-        background: "#153259",
-        color: "whitesmoke",
-        padding: "1em",
-        width: "fit-content",
-        borderRadius: "8px",
-        margin: "1em",
-      }}
-    >
-      <form onSubmit={event => handleSubmit(event)}>
-        <label htmlFor="Title">Change document title</label>
-        <input
-          name="Title"
-          type="text"
-          value={value}
-          onChange={event => setValue(event.target.value)}
-        />
-        <button type="submit" style={{ margin: "8px" }}>
-          Submit
-        </button>
-      </form>
-      <p style={{ margin: "0px" }}>Current title: {title}</p>
-    </div>
-  )
-}
-
 export const AnnoyingAlertComponent = () => {
   const [active, setActive] = useState(false)
 
@@ -140,3 +100,5 @@ export const AnnoyingAlertComponent = () => {
     </div>
   )
 }
+
+export const MyTitleChanger = loadable(() => import("./loadable"))
---
+++ src/components/loadable.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+
+const TitleChanger = () => {
+  const [title, setTitle] = useState(document.title)
+  const [value, setValue] = useState("")
+
+  useEffect(() => {
+    document.title = title
+  }, [title])
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setTitle(value)
+  }
+
+  return (
+    <div
+      style={{
+        background: "#153259",
+        color: "whitesmoke",
+        padding: "1em",
+        width: "fit-content",
+        borderRadius: "8px",
+        margin: "1em",
+      }}
+    >
+      <form onSubmit={event => handleSubmit(event)}>
+        <label htmlFor="Title">Change document title</label>
+        <input
+          name="Title"
+          type="text"
+          value={value}
+          onChange={event => setValue(event.target.value)}
+        />
+        <button type="submit" style={{ margin: "8px" }}>
+          Submit
+        </button>
+      </form>
+      <p style={{ margin: "0px" }}>Current title: {title}</p>
+    </div>
+  )
+}
+
+export default TitleChanger
